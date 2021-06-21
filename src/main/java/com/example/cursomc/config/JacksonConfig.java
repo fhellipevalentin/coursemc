7c942b7adbb6ca0d26b6f183901d76640ecfa757
fhellipevalentin <fhellipereis@outlook.com>
2021-06-21 13:52:07 -0300

    Adicionando Senha a Cliente

--- src/main/java/com/example/cursomc/config/JacksonConfig.java
+++ src/main/java/com/example/cursomc/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package com.example.cursomc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.example.cursomc.domain.PagamentoComBoleto;
 import com.example.cursomc.domain.PagamentoComCartao;
@@ -21,5 +23,9 @@ public class JacksonConfig {
 			}
 		};
 		return builder;
+	}
+	@Bean
+	public JavaMailSender jMS (){
+		return new JavaMailSenderImpl();
 	}
 }
--- src/main/java/com/example/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/example/cursomc/dto/ClienteNewDTO.java
@@ -26,6 +26,9 @@ public class ClienteNewDTO implements Serializable{
 	
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	private String senha;
+	
 	private String logradouro;
 	
 	@NotEmpty(message="Preenchimento Obrigatório")
@@ -151,5 +154,13 @@ public class ClienteNewDTO implements Serializable{
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 }
